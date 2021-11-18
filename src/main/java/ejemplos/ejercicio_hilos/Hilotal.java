51afdd61609e21f254df98538f0c1fe83e3f6a78
Javier <javmar421@alu.centroafuera.es>
2021-11-18 13:12:27 +0100

    hilos apuntes

---
+++ src/main/java/ejemplos/ejercicio_hilos/Hilotal.java
@@ -0,0 +1,49 @@
+package ejemplos.ejercicio_hilos;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+//hilo
+public class Hilotal extends Thread {
+
+    Random r = new Random(100);
+    Random i = new Random(1000);
+    Random precio = new Random(100);
+    int porcen = 100;
+    boolean bool = false;
+    public int contador = 0;
+    public final int numero;
+    ArrayList Lista = new ArrayList();
+    public long tiempoFinalizacion;
+
+
+    public Hilotal(int numero) {
+        this.numero = numero;
+    }
+
+    @Override
+    public void run() {
+        long comienzo = System.currentTimeMillis();
+        while (!bool) {
+            try {
+                Thread.sleep(1000);
+                porcen -= 1;
+                int num = r.nextInt();
+                if (num < porcen) {
+                    bool = false;
+                    String item = "item" + i.nextInt();
+                    Lista.add(precio.nextInt(), item);
+                }
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        finalistas.listaHilos.add(this);
+        System.out.println("Soy el hilo " + numero + " y he dormido " + contador + " veces");
+
+    }
+
+
+}
---
+++ src/main/java/teoria/Hilos.java
@@ -0,0 +1,35 @@
+package teoria;
+
+import ejemplos.ejemplo_hilos.Main;
+
+public class Hilos {
+    public static void main(String args[]) throws InterruptedException {
+        //crear 100 hilos
+        for (int i = 0; i < 10; i++) {
+            Hilo h = new Hilo(i);
+            //start->Override
+            h.start();
+        }
+    }
+}
+class Hilo extends Thread {
+
+    public Integer num;
+
+    public Hilo(Integer num){
+        this.num = num;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Soy el hilo " + num + " y me voy a dormir.");
+        try {
+            int num=Main.getRandomTimeInMills();
+            Thread.sleep(num);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Soy el hilo " + num + " he dormido "+num+" milis y me he despertado.");
+
+    }
+}
