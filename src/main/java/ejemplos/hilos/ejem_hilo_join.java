e0a790fa21804ea2336f626c8a88c56dec6f4049
Javier <javmar421@aul.centroafuera.es>
2021-11-24 00:05:58 +0100

    hilos ejercicios

--- src/main/java/ejemplos/hilos/ejem_hilo_join.java
+++ src/main/java/ejemplos/hilos/ejem_hilo_join.java
@@ -13,9 +13,9 @@ import java.util.Random;
 
             //se puede quitar el try catch poniendo el throw InterruptedException
                 h1.start();
+                h2.start();
                 //join sirve para ordenar entradas de hilos
                 h1.join();
-                h2.start();
                 h2.join();
 
 
